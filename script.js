4158428ea92dfe964623dbe4225ec4a024563d2d
Tensen <thienmpham@gmail.com>
2026-02-27 12:24:58 -0500

    Revised planning
    I returned to the pre planning stage as my logic
    was not coherent. I am using pseudocode in word doc

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-function setGameboard() {
+// Generate gameboard 3x3
+function generateGame() {
   let gameboard = {
-    tiles: ["x"],
+    tiles: [],
+    players: { player1: "X", player2: "O" },
+    // Player chooses choices X or O
+    choosePlayer() {
+      let choice;
+      choice = this.players.player1;
+    },
   };
 }
 
-function setPlayers(name) {
-  let players = {
-    name: name,
-    choice: ["x", "o"],
-  };
-}
-function playGame() {
-  //
-}
+// Player chooses which tile to place choice
 
-setGameboard();
+// Check to see if a choice is in tile
+// IF tile is empty
+// THEN place choice inside tile
+// ELSE player chooses tile again
+
+// Player places choice inside chosen tile
+
+// Check to see if 3 in a row of said choice
+// IF there is 3 in a row
+// THEN player wins and game ends!
+// ELSE its other players turn
