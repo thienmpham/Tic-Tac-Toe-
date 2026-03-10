cfdc5a884c8cecbf969fdd9b6f2469dfb17198e5
Tensen <thienmpham@gmail.com>
2026-03-09 22:30:57 -0400

    Trying to figure out how to make bingo work in console

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
-// Generate gameboard 3x3
+// 1.Generate gameboard 3x3
 function renderGame() {
   let gameboard = {
     tiles: [],
     players: { player1: "X", player2: "O" },
-    // Player chooses choices X or O
+    // 2.Player chooses choices X or O
     choosePlayer() {
+      let counter = null;
       let choice;
-      choice = this.players.player1;
+
+      if (counter == null) {
+        // User chooses player type
+        counter = 0;
+      }
+
+      if (counter == 0) {
+        choice = this.players.player1;
+        counter = 1;
+      } else {
+        choice == this.players.player2;
+        counter = 0;
+      }
+
+      // counter++;
+      return choice;
     },
   };
-  console.log(gameboard);
+
+  populateTiles(gameboard);
   return gameboard;
 }
 renderGame();
 
-// Player chooses which tile to place choice
-function populateTiles() {
-  console.log();
-  // Check to see if a choice is in tile
-  // IF tile is empty
-  // THEN place choice inside tile
-  // ELSE player chooses tile again
+function populateTiles(gameboard) {
+  let choice = gameboard.choosePlayer();
+  let tiles = gameboard.tiles;
+  console.log(tiles, choice);
+  // 3.Player chooses which tile to place choice
+  let tileChoice = tiles[chooseTile()];
+
+  // 4 Check to see if a choice is in tile
+  // 4.1 IF tile is empty
+  // 4.2 THEN place choice inside tile
+  // 4.3 ELSE player chooses tile again
+  if (tileChoice !== null) {
+    tiles.splice(tileChoice, 1, choice);
+  } else {
+    console.log("invalid tile choice.. Choose again.");
+  }
 }
 
-// Player places choice inside chosen tile
+function chooseTile(num) {
+  // 0-9
+  if (num >= 0 && num < 9) {
+    console.log("Nice Tile!");
+    return num;
+  } else {
+    console.log("ERROR: tile choice is invalid.");
+  }
+}
+// 5 Player places choice inside chosen tile
 
-// Check to see if 3 in a row of said choice
-// IF there is 3 in a row
-// THEN player wins and game ends!
-// ELSE its other players turn
+// 6 Check to see if 3 in a row of said choice
+// 6.1 IF there is 3 in a row
+// 6.2 THEN player wins and game ends!
+// 6.3 ELSE its other players turn
