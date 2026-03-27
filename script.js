3ad33cbe644e924549b456838b8ae1536188e480
Tensen <thienmpham@gmail.com>
2026-03-27 14:54:12 -0400

    Figuring out changing choice from X to O

--- script.js
+++ script.js
@@ -6,67 +6,65 @@ function renderGame() {
     players: { player1: "X", player2: "O" },
     // 2.Player chooses choices X or O
     choosePlayer() {
-      let counter = null;
+      let counter;
       let choice;
 
       if (counter == null) {
         // User chooses player type
-        counter = 0;
+        counter = 1;
       }
 
-      if (counter == 0) {
+      if (counter == 1) {
         choice = this.players.player1;
-        counter = 1;
+        counter = 2;
       } else {
         choice = this.players.player2;
-        counter = 0;
+        counter = 1;
       }
-
-      // counter++;
       return choice;
     },
   };
 
-  checkTilesIfEmpty(gameboard);
+  gameboard.tiles = chooseTile(gameboard);
   return gameboard;
 }
 renderGame();
 
-function checkTilesIfEmpty(gameboard) {
-  let tiles = gameboard.tiles;
-  // 3.Player chooses which tile to place choice
-  let tileChoice = tiles[chooseTile()];
+function chooseTile(gameboard) {
+  let tilesArray = gameboard.tiles;
 
+  // 0-9
+  // 3.Player chooses which tile to place choice
+  let num = prompt("Pick a tile #1-9");
+  let empty = checkTilesIsEmpty(tilesArray, num);
+
+  if (num > 0 && num < 9 && empty == true) {
+    console.log(`You chose Tile #${num}!`);
+    return populateTiles(gameboard, tilesArray, num);
+  } else {
+    console.log("ERROR: tile choice is invalid.");
+    chooseTile();
+  }
+}
+function checkTilesIsEmpty(tilesArray, num) {
   // 4 Check to see if a choice is in tile
   // 4.1 IF tile is empty
   // 4.2 THEN place choice inside tile
   // 4.3 ELSE player chooses tile again
-  if (tileChoice !== null) {
+  if (tilesArray[num] == null) {
     console.log("Tile is EMPTY!");
-    populateTiles(gameboard, tiles, tileChoice);
     return true;
     // tiles.splice(tileChoice, 1, choice);
   } else {
     console.log("Tile is NOT empty. Choose again.");
-    chooseTile();
-  }
-}
-
-function chooseTile() {
-  // 0-9
-  let num = prompt("Pick a tile #1-9");
-  if (num > 0 && num < 9) {
-    console.log(`You chose Tile #${num}!`);
-    return num;
-  } else {
-    console.log("ERROR: tile choice is invalid.");
+    return false;
   }
 }
 
 // 5 Player places choice inside chosen tile
-function populateTiles(gameboard, tiles, tileChoice) {
+function populateTiles(gameboard, tiles, num) {
   let choice = gameboard.choosePlayer();
-  tiles.splice(tileChoice, 1, choice);
+  tiles.splice(tiles[num], 1, choice);
   console.log(tiles);
   return tiles;
 }
